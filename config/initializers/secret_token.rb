3f300854e34a9f04a8fa94b810614d5ff1a2fda2
chintanparikh <chintanparikh@gatech.edu>
2013-08-02 13:46:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterLists::Application.config.secret_token = 'e4b64b7fc5ed9615355c85d7c4adeb7cfdd61029771e55d740822aef5948f74f0ce1877349b95aa8478594c6f41e26b09f066143d5ffed0f588f9cdd3aacdcdf'
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "avhkD8Dzuj5wzpsAjYLcag"
+  config.consumer_secret = "DFUUxS3s85B6hBrZOpQzowPOt9v14NvU9gWQoGgpnbw"
+  config.oauth_token = "1640927101-j318x16Lg9OZ6ZeFd9NonArYoFQ4wW3aoi19uXA"
+  config.oauth_token_secret = "PwjeOg5UvdCWsv2TomAfzgMy65LNHO3HiRkpSPAsWrQ"
+end
