0e63eec3101d3899fdfdd45d1b217b7a18ada09d
Chintan Parikh <chintanparikh@gatech.edu>
2014-02-27 19:01:05 -0500

    Update twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key = "avhkD8Dzuj5wzpsAjYLcag"
-  config.consumer_secret = "DFUUxS3s85B6hBrZOpQzowPOt9v14NvU9gWQoGgpnbw"
-  config.oauth_token = "1640927101-0lru5rVvm6pG1gWchaYEb9BqZTs23qxstU5h9z6"
-  config.oauth_token_secret = "ThsOLR9bwdDM970MxvzVSpGnCIp7M9U7Fy9qZqAUPlk"
+  config.consumer_key = ""
+  config.consumer_secret = ""
+  config.oauth_token = ""
+  config.oauth_token_secret = ""
 end
